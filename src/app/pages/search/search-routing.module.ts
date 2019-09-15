ca21d0e65c1633774edbc821300c708b10899cad
chema <chemabravoh@hotmail.com>
2019-09-15 13:52:54 +0200

    estructura basica de formulario de búsqueda y sus resultados

--- src/app/pages/search/search-routing.module.ts
+++ src/app/pages/search/search-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search.component';
+import { FormComponent } from './components/form/form.component';
+import { ResultComponent } from './components/result/result.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: SearchComponent
+    component: SearchComponent,
+    children: [
+      {
+        path: 'result',
+        component: ResultComponent
+      }
+    ]
   }
 ];
 
--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
+import { FormComponent } from './components/form/form.component';
+import { ResultComponent } from './components/result/result.component';
 
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [SearchComponent, FormComponent, ResultComponent],
   imports: [
     CommonModule,
     SearchRoutingModule
